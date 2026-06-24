215a7d1d186dbd74be6b8abe11bf016c5463802a
skem-skem <lazenda001@gmail.com>
2026-06-24 16:27:35 +0200

    update dependencies / frontend changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["xxxxx.supabase.co"],
+  }
 };
 
 export default nextConfig;
