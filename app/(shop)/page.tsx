ce8907c52a453635cd1d6c871dd9305656e61cdb
skem-skem <lazenda001@gmail.com>
2026-06-28 14:10:59 +0200

    fixed wrapp

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,11 +1,14 @@
 import Products from "@/app/components/Products";
 import Hero from "../components/Hero";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Products />
+      <Suspense>
+        <Products />
+      </Suspense>
     </>
 
   );
